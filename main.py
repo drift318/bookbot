d537265ab20f10c605d39c66c4c1fac4c2fc15d1
Michael <56277222+drift318@users.noreply.github.com>
2025-11-01 22:09:40 -0500

    added system args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import stats
+import sys
 
 def get_book_text(file_path):
     with open(file_path, 'r') as file:
@@ -6,23 +7,27 @@ def get_book_text(file_path):
         return file_content
 
 def main():
-    book_path = 'books/frankenstein.txt'
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     return book_text
 
 
     
 if __name__ == "__main__":
-    characters = main()  
+    try:
+        characters = main()
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)  
     counters = stats.char_count(characters)
     words = stats.word_count(characters)
     sorted_list = stats.sort_count(counters)
 
     
-    print('''============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+    print(f'''============ BOOKBOT ============
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------''')
     print(f'Found {words} total words')
     print('''--------- Character Count -------''')
     for i in sorted_list:
-        print(f'{i['char']}: {i['num']}')
+        print(f"{i['char']}: {i['num']}")
